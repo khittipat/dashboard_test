94941157c16ae36f61483fd89645e1ca112cd8ec
khittipatTong <75748939+khittipat@users.noreply.github.com>
2023-08-20 11:54:43 +0700

    revise some overflow text in total sersors component

--- src/components/TotalSensor.jsx
+++ src/components/TotalSensor.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 
 const GraphCard = ({ title, data }) => {
   return (
-    <div className='bg-slate-900 shadow-lg p-2 h-64 border-2 border-slate-600'>
+    <div className='bg-slate-900 shadow-lg p-2 h-64 border-2 border-slate-600 overflow-auto'>
         <div>
-            <a className='text-white text-xs font-semibold mb-4'>{`Total Sensor(s)`}</a>
+            <a className='text-white text-xs font-semibold mb-4 text-ellipsis'>{`Total Sensor(s)`}</a>
         </div>
-        <div className='h-max text-center'>
-            <b className='text-green-400 text-[96px] md:text-[128px] lg:text-[144px]'>8</b>
+        <div className='text-center mt-10 sm:mt-4 md:mt-0'>
+            <b className='text-green-400 text-[64px] sm:text-[96px] md:text-[128px] lg:text-[132px]'>8</b>
         </div>
     </div>
   );
